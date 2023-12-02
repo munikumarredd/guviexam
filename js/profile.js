802ef1161c6f0f585064c50a21add7eac5a76bba
munikumarredd <151002717+munikumarredd@users.noreply.github.com>
2023-12-02 14:40:16 +0530

    Initial commit

---
+++ js/profile.js
@@ -0,0 +1,31 @@
+// Assume you have stored the user's email in local storage during login
+var userEmail = localStorage.getItem(userEmail);
+
+$(document).ready(function () {
+    // Assume you have stored the user's email in local storage during login
+    var userEmail = localStorage.getItem("userEmail");
+
+    // Fetch user details based on email
+    $.ajax({
+        type: 'GET',
+        url: '/Guvi_Assignment_muni/php/profile.php',
+        data: { email: userEmail },
+        success: function (response) {
+            var user = JSON.parse(response);
+
+            if (user.error) {
+                // Handle the case where the user is not found
+                console.error('User not found:', user.error);
+            } else {
+                // Update the HTML with user details
+                $('#firstName').text(user.firstName);
+                $('#lastName').text(user.lastName);
+                $('#email').text(user.email);
+                $('#phoneNumber').text(user.phoneNumber);
+            }
+        },
+        error: function (xhr, status, error) {
+            console.error('AJAX Error:', status, '-', error);
+        }
+    });
+});
---
+++ php/profile.php
@@ -0,0 +1,27 @@
+<?php
+$conn = new mysqli('localhost', 'root', '', 'guvi_assignment_muni');
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $email = $_GET["email"];
+    $sql = "SELECT * FROM user WHERE email = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+        echo json_encode($user);
+    } else {
+        // Return a JSON response indicating that the user was not found
+        echo json_encode(["error" => "User not found"]);
+    }
+
+    $stmt->close();
+}
+
+$conn->close();
